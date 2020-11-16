3dd37da935e101ef9c7f4c8b12079ecd6840f7c6
esynr3z <esynr3z@gmail.com>
2020-11-16 13:51:19 +0300

    Add templates for some modules

---
+++ corsair/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""CorSaiR is a control and status register (CSR) map generator for FPGA/ASIC projects.
+It generates HDL code, documentation and other artifacts from CSR map description file.
+"""
+
+__version__ = "0.0.0"
