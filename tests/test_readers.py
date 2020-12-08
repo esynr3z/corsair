11b8c5f7d2eee4b59131ac4c05319d3dfe854a64
esynr3z <esynr3z@gmail.com>
2020-12-08 09:57:27 +0300

    Prettify readers test

--- tests/test_readers.py
+++ tests/test_readers.py
@@ -12,13 +12,14 @@ class TestJSONReader:
     """Class 'JSONReader' testing."""
 
     @pytest.fixture()
-    def json_file(self):
+    def input_file(self):
         return 'tests/data/map.json'
 
-    def test_read(self, json_file):
+    def test_read(self, input_file):
         """Test of reading JSON file with CSR map."""
+        print('input_file:', input_file)
         reader = JSONReader()
-        rmap = reader(json_file)
+        rmap = reader(input_file)
         print(rmap.config)
         print(rmap)
         assert rmap.config['read_filler'].value == 0xDEADBEEF and \
