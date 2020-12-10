c7ebf68647181e43a36ffa2ae62b6641b935f6d6
esynr3z <esynr3z@gmail.com>
2020-12-10 14:38:21 +0300

    Rework of application CLI

--- corsair/__main__.py
+++ corsair/__main__.py
@@ -9,76 +9,145 @@ import os
 import argparse
 import corsair
 
+__all__ = ['main']
 
-def create_parser():
-    """Create program argument parser."""
-    # inherit ArgumentParser to override the behaviour of error method
-    class ArgumentParser(argparse.ArgumentParser):
-        def error(self, message):
-            self.print_help(sys.stderr)
-            self.exit(2, '\n%s: error: %s\n' % (self.prog, message))
+writers_by_name = {
+    'JSONWriter': corsair.JSONWriter(),
+    'YAMLWriter': corsair.YAMLWriter()
+}
 
+writers_by_ext = {
+    '.json': writers_by_name['JSONWriter'],
+    '.yaml': writers_by_name['YAMLWriter'],
+    '.yml': writers_by_name['YAMLWriter']
+}
+
+readers_by_name = {
+    'JSONReader': corsair.JSONReader(),
+    'YAMLReader': corsair.YAMLReader()
+}
+
+readers_by_ext = {
+    '.json': readers_by_name['JSONReader'],
+    '.yaml': readers_by_name['YAMLReader'],
+    '.yml': readers_by_name['YAMLReader']
+}
+
+
+def arg_input(arg):
+    """Validate input argument file[,ReaderClassName] and convert it to lambda function."""
+    args = [x for x in arg.split(',')]
+    if len(args) == 1:
+        # use extension to choose agent
+        _, csr_ext = os.path.splitext(args[0])
+        csr_ext = csr_ext.lower()
+        try:
+            readers_by_ext[csr_ext]  # check if exists
+            return lambda: readers_by_ext[csr_ext](args[0])
+        except KeyError:  # unknown extension
+            raise argparse.ArgumentError()
+    else:
+        # use provided name for the class
+        try:
+            readers_by_name[args[1]]  # check if exists
+            return lambda: readers_by_name[args[1]](args[0])
+        except KeyError:  # unknown class name
+            raise argparse.ArgumentError()
+
+
+def arg_output(arg):
+    """Validate output argument file[,WriterClassName] and convert it to lambda function."""
+    args = [x for x in arg.split(',')]
+    if len(args) == 1:
+        # use extension to choose agent
+        _, csr_ext = os.path.splitext(args[0])
+        csr_ext = csr_ext.lower()
+        try:
+            writers_by_ext[csr_ext]  # check if exists
+            return lambda rmap: writers_by_ext[csr_ext](args[0], rmap)
+        except KeyError:  # unknown extension
+            raise argparse.ArgumentError()
+    else:
+        # use provided name for the class
+        try:
+            writers_by_name[args[1]]  # check if exists
+            return lambda rmap: writers_by_name[args[1]](args[0], rmap)
+        except KeyError:  # unknown class name
+            raise argparse.ArgumentError()
+
+
+class ArgumentParser(argparse.ArgumentParser):
+    """Inherit ArgumentParser to override the behaviour of error method."""
+    def error(self, message):
+        self.print_help(sys.stderr)
+        self.exit(2, '\n%s: error: %s\n' % (self.prog, message))
+
+
+def parse_arguments():
+    """Parse and validate arguments."""
     parser = ArgumentParser(prog=corsair.__title__,
                             description=corsair.__description__)
     parser.add_argument('-v', '--version',
                         action='version',
                         version='%(prog)s v' + corsair.__version__)
-    parser.add_argument(dest='csr_path',
-                        metavar='CSR_FILE',
-                        help='read CSR map from CSR_FILE')
-    parser.add_argument('--json',
-                        dest='json_path',
-                        metavar='FILE',
-                        help='write CSR map to JSON FILE')
-    parser.add_argument('--yaml',
-                        dest='yaml_path',
-                        metavar='FILE',
-                        help='write CSR map to YAML FILE')
-    parser.add_argument('--verilog',
-                        dest='verilog_path',
-                        metavar='FILE',
-                        help='write CSR map to Verilog FILE')
-    return parser
+    parser.add_argument('-i', '--input',
+                        type=arg_input,
+                        metavar='file[,ReaderClassName]',
+                        dest='reader',
+                        help='read CSR map from file')
+    parser.add_argument('-o', '--output',
+                        type=arg_output,
+                        metavar='file[,WriterClassName]',
+                        nargs='+',
+                        dest='writers',
+                        help='write output to file(s)')
+    parser.add_argument('-t', '--template',
+                        type=arg_output,
+                        metavar='file[,WriterClassName]',
+                        dest='template_writer',
+                        help='write CSR map template to file')
+
+    # check if no arguments provided
+    if len(sys.argv) == 1:
+        parser.parse_args(['--help'])
+        # argparse will raise SystemExit here
+
+    # get arguments namespace
+    args = parser.parse_args()
+
+    # check for conflicts
+    if not args.reader and args.writers:
+        parser.error("Not able to use -o/--output without -i/--input argument!")
+
+    return args
 
 
 def main():
     """Program main."""
     # parse arguments
-    parser = create_parser()
-    if len(sys.argv) == 1:  # if no arguments
-        parser.parse_args(['--help'])
-        # argparse will raise SystemExit here
-    else:
-        args = parser.parse_args()
+    args = parse_arguments()
 
-    # prepare list of writers to use
-    writers = {}
-    output_paths = {}
-    if args.json_path:
-        writers['json'] = corsair.JSONWriter()
-        output_paths['json'] = args.json_path
-    if args.yaml_path:
-        writers['yaml'] = corsair.YAMLWriter()
-        output_paths['yaml'] = args.yaml_path
-    if args.verilog_path:
-        writers['verilog'] = lambda **args: print("Sorry, Verilog writer is not implemented yet,"
-                                                  "'--verilog' key will be ignored.")
-        output_paths['verilog'] = args.verilog_path
+    # create template if needed
+    if args.template_writer:
+        regs = [corsair.Register('spam', ' Register spam', 0),
+                corsair.Register('eggs', ' Register eggs', 4)]
+        regs[0].add_bfields([
+            corsair.BitField('foo', 'Bit field foo', lsb=0, width=7, access='rw', initial=42),
+            corsair.BitField('bar', 'Bit field bar', lsb=24, width=1, access='wo', modifiers=['self_clear'])
+        ])
+        regs[1].add_bfields(corsair.BitField('baz', 'Bit field baz', lsb=16, width=16, access='ro'))
+        rmap = corsair.RegisterMap()
+        rmap.add_regs(regs)
+        args.template_writer(rmap)
 
     # parse CSR file
-    _, csr_file_ext = os.path.splitext(args.csr_path)
-    csr_file_ext = csr_file_ext.lower()
-    if csr_file_ext == '.json':
-        reader = corsair.JSONReader()
-    elif csr_file_ext in ['.yaml', '.yml']:
-        reader = corsair.YAMLReader()
-    else:
-        parser.error("CSR_FILE '%s' has unknown extension '%s'!" % (args.csr_path, csr_file_ext))
-    rmap = reader(args.csr_path)
+    if args.reader:
+        rmap = args.reader()
 
     # create all output artifacts
-    for k in writers.keys():
-        writers[k](path=output_paths[k], rmap=rmap)
+    if (args.writers):
+        for writer in args.writers:
+            writer(rmap)
 
     sys.exit(0)
 
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -37,17 +37,38 @@ class TestCLI:
         assert exit_code == 0
         assert 'usage: corsair' in captured.out
 
-    def test_wrong_csr(self, capsys):
-        """Application with wrong CSR should exit with error."""
-        exit_code = self._run_cli(['aaa'])
+    def test_input_bad_ext(self, capsys):
+        """CSR file without extension should lead to exit with error."""
+        exit_code = self._run_cli(['-i', 'aaa'])
         captured = capsys.readouterr()
         assert exit_code == 2
-        assert 'corsair: error:' in captured.err
+        assert 'corsair: error: argument -i/--input:' in captured.err
+
+    def test_input_bad_name(self, capsys):
+        """CSR file with wrong reader class name should lead to exit with error."""
+        exit_code = self._run_cli(['-i', 'aaa.json,AAAReader'])
+        captured = capsys.readouterr()
+        assert exit_code == 2
+        assert 'corsair: error: argument -i/--input:' in captured.err
+
+    def test_output_bad_ext(self, capsys):
+        """Path for an output file without extension should lead to exit with error."""
+        exit_code = self._run_cli(['-o', 'aaa'])
+        captured = capsys.readouterr()
+        assert exit_code == 2
+        assert 'corsair: error: argument -o/--output:' in captured.err
+
+    def test_output_bad_name(self, capsys):
+        """Path for an output file with wrong writer class name should lead to exit with error."""
+        exit_code = self._run_cli(['-o', 'aaa.json,AAAWriter'])
+        captured = capsys.readouterr()
+        assert exit_code == 2
+        assert 'corsair: error: argument -o/--output:' in captured.err
 
     def test_read_csr(self, test_data_dir, capsys):
         """Application with correct CSR should exit without error."""
         csr_file = test_data_dir.join('map.json')
-        exit_code = self._run_cli([str(csr_file)])
+        exit_code = self._run_cli(['-i', str(csr_file)])
         captured = capsys.readouterr()
         assert exit_code == 0
         assert 'Read register map ... OK' in captured.out
@@ -57,8 +78,8 @@ class TestCLI:
         csr_file = test_data_dir.join('map.json')
         output_json = tmpdir.join('map_out.json')
         output_yaml = tmpdir.join('map_out.yaml')
-        self._run_cli([str(csr_file), '--yaml=%s' % output_yaml])
-        self._run_cli([str(output_yaml), '--json=%s' % output_json])
+        self._run_cli(['-i', str(csr_file), '-o', str(output_yaml)])
+        self._run_cli(['-i', str(output_yaml), '-o', str(output_json)])
         orig_rmap = corsair.JSONReader()(str(csr_file))
         test_rmap = corsair.JSONReader()(str(output_json))
         assert orig_rmap == test_rmap
@@ -68,9 +89,18 @@ class TestCLI:
         csr_file = test_data_dir.join('map.json')
         output_json = tmpdir.join('map_out.json')
         output_yaml = tmpdir.join('map_out.yaml')
-        self._run_cli([str(csr_file), '--yaml=%s' % output_yaml, '--json=%s' % output_yaml])
-        self._run_cli([str(output_yaml), '--json=%s' % output_json])
+        self._run_cli(['-i', str(csr_file), '-o', str(output_yaml), str(output_yaml)])
+        self._run_cli(['-i', str(output_yaml), '-o', str(output_json)])
         orig_rmap = corsair.JSONReader()(str(csr_file))
         json_rmap = corsair.JSONReader()(str(output_json))
         yaml_rmap = corsair.YAMLReader()(str(output_yaml))
         assert orig_rmap == json_rmap == yaml_rmap
+
+    def test_template_write(self, tmpdir, capsys):
+        """Template file should be generated."""
+        template_json = tmpdir.join('template')
+        self._run_cli(['-t', '%s,JSONWriter' % str(template_json)])
+        exit_code = self._run_cli(['-i', '%s,JSONReader' % str(template_json)])
+        captured = capsys.readouterr()
+        assert exit_code == 0
+        assert 'Read register map ... OK' in captured.out
