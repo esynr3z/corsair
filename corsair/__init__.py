206f2286bf04ccd135944a37309c94bfae001524
esynr3z <esynr3z@gmail.com>
2024-10-09 22:15:49 +0300

    feat: replace setup.py with poetry (#71)

--- corsair/__init__.py
+++ corsair/__init__.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 """Corsair is a control and status register (CSR) map generator for HDL projects.
+
 It generates HDL code, documentation and other artifacts from CSR map description file.
 """
+
+from __future__ import annotations
 
 __title__ = "corsair"
 __description__ = "Control and status register (CSR) map generator for HDL projects."
 
-try:
-    from ._version import version as __version__
-except (ImportError, ModuleNotFoundError) as e:
-    __version__ = 'git-latest'
-
-from . import config
-from .enum import EnumValue
+from . import config, generators
 from .bitfield import BitField
+from .enum import EnumValue
 from .reg import Register
 from .regmap import RegisterMap
-from . import generators
+from .version import __version__
+
+__all__ = [
+    "__version__",
+    "RegisterMap",
+    "Register",
+    "EnumValue",
+    "BitField",
+    "config",
+    "generators",
+]
---
+++ corsair/version.py
@@ -0,0 +1,5 @@
+"""Separate module to contain version and avoid circular dependencies."""
+
+from __future__ import annotations
+
+__version__ = "0.0.0"  # 0.0.0 is standard placeholder for poetry-dynamic-versioning
